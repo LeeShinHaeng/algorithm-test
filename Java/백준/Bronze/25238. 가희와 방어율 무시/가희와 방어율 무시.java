2f3f2c9032274b1c74161b8d289be19eb906e016
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-06-28 09:03:11 +0900

    [Bronze IV] Title: 가희와 방어율 무시, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ Java/백준/Bronze/25238. 가희와 방어율 무시/가희와 방어율 무시.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+
+        float real = a - ((float) (a * b) / 100);
+        System.out.println(real >= 100 ? 0 : 1);
+    }
+}
