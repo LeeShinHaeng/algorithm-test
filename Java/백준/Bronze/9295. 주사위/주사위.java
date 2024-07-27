2fb2ab98d358ca8d101d289f0c02f35cf13b9892
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-07-27 09:11:32 +0900

    [Bronze IV] Title: 주사위, Time: 132 ms, Memory: 14572 KB -BaekjoonHub

--- Java/백준/Bronze/9295. 주사위/주사위.java
+++ Java/백준/Bronze/9295. 주사위/주사위.java
@@ -5,14 +5,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int size = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < size; i++) {
             String[] input = br.readLine().split(" ");
             int a = Integer.parseInt(input[0]);
             int b = Integer.parseInt(input[1]);
-
-            System.out.println("Case " + (i + 1) + ": " + (a + b));
+            sb.append("Case ").append(i + 1).append(": ").append(a + b).append("\n");
         }
+        System.out.println(sb);
     }
 }
