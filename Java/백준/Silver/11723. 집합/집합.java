4b9e2ec354714a3c7c0ab13ed2a700098a538c5c
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-07-30 08:33:37 +0900

    [Silver V] Title: 집합, Time: 1424 ms, Memory: 308148 KB -BaekjoonHub

--- Java/백준/Silver/11723. 집합/집합.java
+++ Java/백준/Silver/11723. 집합/집합.java
@@ -6,7 +6,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Set<Integer> set = new HashSet<>();
@@ -16,32 +15,32 @@ public class Main {
         for (int i = 0; i < k; i++) {
             String[] s = br.readLine().split(" ");
             char c = s[0].charAt(1);
-            switch (c) {
-                case 'd':
+            switch (s[0]) {
+                case "add":
                     set.add(Integer.parseInt(s[1]));
                     break;
-                case 'e':
+                case "remove":
                     set.remove(Integer.parseInt(s[1]));
                     break;
-                case 'h':
+                case "check":
                     if (set.contains(Integer.parseInt(s[1]))) {
                         sb.append(1).append("\n");
                     } else {
                         sb.append(0).append("\n");
                     }
                     break;
-                case 'o':
+                case "toggle":
                     if (set.contains(Integer.parseInt(s[1]))) {
                         set.remove(Integer.parseInt(s[1]));
                     } else {
                         set.add(Integer.parseInt(s[1]));
                     }
                     break;
-                case 'l':
+                case "all":
                     set.addAll(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                             11, 12, 13, 14, 15, 16, 17, 18, 19, 20));
                     break;
-                case 'm':
+                case "empty":
                     set.clear();
                     break;
             }
