cf07a22b336ae029fc7060fb2ea7de69e668297e
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-08-05 09:06:35 +0900

    [Silver V] Title: 줄 세우기, Time: 100 ms, Memory: 14108 KB -BaekjoonHub

--- Java/백준/Silver/11536. 줄 세우기/줄 세우기.java
+++ Java/백준/Silver/11536. 줄 세우기/줄 세우기.java
@@ -6,28 +6,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int flag;
+        boolean flag;
 
         String s1 = br.readLine();
         String s2 = br.readLine();
         if (s1.compareTo(s2) < 0) {
-            flag = -1;
+            flag = false;
         } else {
-            flag = 1;
+            flag = true;
         }
 
         for (int i = 0; i < n - 2; i++) {
             s1 = s2;
             s2 = br.readLine();
 
-            if ((s1.compareTo(s2) < 0 && flag == 1) ||
-                    (s1.compareTo(s2) > 0 && flag == -1)) {
+            if ((s1.compareTo(s2) < 0 && flag) ||
+                    (s1.compareTo(s2) > 0 && (! flag))) {
                 System.out.println("NEITHER");
                 return;
             }
         }
 
-        if (flag == 1) {
+        if (flag) {
             System.out.println("DECREASING");
             return;
         }
