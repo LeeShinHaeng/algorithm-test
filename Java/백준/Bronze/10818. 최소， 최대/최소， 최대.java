e8d76855c6d2c99c70e4ff8138d1faa77f046c09
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-08-22 10:36:13 +0900

    [Bronze III] Title: 최소, 최대, Time: 464 ms, Memory: 106580 KB -BaekjoonHub

--- Java/백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ Java/백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,26 +1,25 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] nums = new int[n];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
         for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
-        }
-        sc.close();
-
-        int min = nums[0];
-        int max = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            if (nums[i] < min) {
-                min = nums[i];
-            } else if (nums[i] > max) {
-                max = nums[i];
+            int tmp = Integer.parseInt(s[i]);
+            if (tmp > max) {
+                max = tmp;
+            }
+            if (tmp < min) {
+                min = tmp;
             }
         }
+
         System.out.println(min + " " + max);
     }
 }
