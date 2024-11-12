d207ce31e3490eae526baea4339ff558bab96a79
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-11-12 11:02:40 +0900

    [Silver II] Title: DFS와 BFS, Time: 264 ms, Memory: 20964 KB -BaekjoonHub

--- Java/백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ Java/백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,67 +1,98 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
+class Node implements Comparable<Node> {
+	int id;
+	List<Node> links;
+	boolean visited;
+
+	Node(int id) {
+		this.id = id;
+		links = new ArrayList<>();
+		visited = false;
+	}
+
+	void link(Node node) {
+		links.add(node);
+	}
+
+	@Override
+	public int compareTo(Node other) {
+		return this.id - other.id;
+	}
+}
+
 public class Main {
-    static int N;
-    static boolean[][] graph;
-    static boolean[] visited;
-    static Queue<Integer> queue = new LinkedList<>();
+	static Node[] nodes;
+	static StringBuilder dfsResult = new StringBuilder();
+	static StringBuilder bfsResult = new StringBuilder();
 
-    static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] input = br.readLine().split(" ");
-        N = Integer.parseInt(input[0]);
-        int m = Integer.parseInt(input[1]);
-        int v = Integer.parseInt(input[2]);
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+		int V = Integer.parseInt(input[2]);
 
-        graph = new boolean[N + 1][N + 1];
-        visited = new boolean[N + 1];
+		nodes = new Node[N + 1];
+		for (int i = 1; i <= N; i++) {
+			nodes[i] = new Node(i);
+		}
 
-        for (int i = 0; i < m; i++) {
-            String[] tmp = br.readLine().split(" ");
-            graph[Integer.parseInt(tmp[0])][Integer.parseInt(tmp[1])] = true;
-            graph[Integer.parseInt(tmp[1])][Integer.parseInt(tmp[0])] = true;
-        }
+		for (int i = 0; i < M; i++) {
+			input = br.readLine().split(" ");
+			int a = Integer.parseInt(input[0]);
+			int b = Integer.parseInt(input[1]);
+			nodes[a].link(nodes[b]);
+			nodes[b].link(nodes[a]);
+		}
 
-        dfs(v);
-        sb.append("\n");
-        visited = new boolean[N + 1];
-        bfs(v);
+		for (int i = 1; i <= N; i++) {
+			Collections.sort(nodes[i].links);
+		}
+		dfs(nodes[V]);
+		for (int i = 1; i <= N; i++) {
+			nodes[i].visited = false;
+		}
+		bfs(nodes[V]);
 
-        bw.write(sb.toString());
-        bw.flush();
-    }
+		System.out.println(dfsResult.toString() + "\n" + bfsResult.toString());
+	}
 
-    private static void dfs(int v) {
-        visited[v] = true;
-        sb.append(v).append(" ");
+	public static void dfs(Node node) {
+		if (node.visited)
+			return;
 
-        for (int i = 0; i <= N; i++) {
-            if (graph[v][i] && !visited[i]) {
-                dfs(i);
-            }
-        }
+		node.visited = true;
+		dfsResult.append(node.id).append(" ");
+		for (Node link : node.links) {
+			dfs(link);
+		}
+	}
 
-    }
+	public static void bfs(Node start) {
+		Queue<Node> queue = new LinkedList<>();
+		start.visited = true;
+		queue.offer(start);
 
-    private static void bfs(int v) {
-        queue.add(v);
-        visited[v] = true;
+		while (!queue.isEmpty()) {
+			Node current = queue.poll();
+			current.visited = true;
+			bfsResult.append(current.id).append(" ");
 
-        while (!queue.isEmpty()) {
-            v = queue.poll();
-            sb.append(v).append(" ");
-
-            for (int i = 0; i <= N; i++) {
-                if (graph[v][i] && !visited[i]) {
-                    queue.add(i);
-                    visited[i] = true;
-                }
-            }
-        }
-    }
+			for (Node link : current.links) {
+				if (link.visited || queue.contains(link)) {
+					continue;
+				}
+				queue.offer(link);
+			}
+		}
+	}
 }
