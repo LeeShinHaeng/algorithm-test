6039fddf5611d930570b65428d8063d2780a6d53
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2024-12-09 11:39:09 +0900

    [level 2] Title: 행렬의 곱셈, Time: 6.60 ms, Memory: 92.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int a = arr1.length;
+        int b = arr1[0].length;
+        int c = arr2.length;
+        int d = arr2[0].length;
+        int[][] result = new int[a][d];
+        
+        for(int i = 0; i < a; i++){
+            for(int j = 0; j < d; j++){
+                for(int k = 0; k < b; k++){
+                    result[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        
+        return result;
+    }
+}
