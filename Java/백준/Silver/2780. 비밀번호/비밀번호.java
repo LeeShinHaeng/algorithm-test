52524c8f08de7b57c9ff36ce48b302d26848422f
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2025-01-06 11:09:05 +0900

    [Silver I] Title: 비밀번호, Time: 144 ms, Memory: 14760 KB -BaekjoonHub

---
+++ Java/백준/Silver/2780. 비밀번호/비밀번호.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int[][] dp = new int[1001][10];
+		Arrays.fill(dp[1], 1);
+		int MOD = 1_234_567;
+
+		for (int i = 2; i <= 1000; i++) {
+			dp[i][0] = dp[i - 1][7] % MOD;
+			dp[i][1] = (dp[i - 1][2] + dp[i - 1][4]) % MOD;
+			dp[i][2] = (dp[i - 1][1] + dp[i - 1][3] + dp[i - 1][5]) % MOD;
+			dp[i][3] = (dp[i - 1][2] + dp[i - 1][6]) % MOD;
+			dp[i][4] = (dp[i - 1][1] + dp[i - 1][5] + dp[i - 1][7]) % MOD;
+			dp[i][5] = (dp[i - 1][2] + dp[i - 1][4] + dp[i - 1][6] + dp[i - 1][8]) % MOD;
+			dp[i][6] = (dp[i - 1][3] + dp[i - 1][5] + dp[i - 1][9]) % MOD;
+			dp[i][7] = (dp[i - 1][4] + dp[i - 1][8] + dp[i - 1][0]) % MOD;
+			dp[i][8] = (dp[i - 1][5] + dp[i - 1][7] + dp[i - 1][9]) % MOD;
+			dp[i][9] = (dp[i - 1][6] + dp[i - 1][8]) % MOD;
+		}
+		
+		for (int t = 0; t < T; t++) {
+			int num = Integer.parseInt(br.readLine());
+			System.out.println((dp[num][0] + dp[num][1] + dp[num][2] + dp[num][3] + dp[num][4] + dp[num][5] + dp[num][6] + dp[num][7] + dp[num][8] + dp[num][9]) % MOD);
+		}
+	}
+}
