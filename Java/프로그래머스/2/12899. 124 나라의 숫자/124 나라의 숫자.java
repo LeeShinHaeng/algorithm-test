b16102b88719638440bb71fe906ad61587523f14
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2025-02-05 17:43:13 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 53.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        
+        while(n > 0){
+            if(n % 3 == 0){
+                answer.append(4);
+                n = n / 3 - 1;
+                continue;
+            }
+            answer.append(n%3);
+            n = n / 3;
+        }
+        
+        return answer.reverse().toString();
+    }
+}
