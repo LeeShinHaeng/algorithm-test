4f3cb74b51c6878eafdc60a3689c1616faab60be
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2025-02-08 19:46:46 +0900

    [Silver IV] Title: 문자열 집합, Time: 380 ms, Memory: 31280 KB -BaekjoonHub

--- Java/백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ Java/백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,34 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) {
-        stringSet();
-    }
-
-    static public void stringSet() {
-        Scanner scanner = new Scanner(System.in);
-
-        int numOfSet = scanner.nextInt();
-        int numOfStrings = scanner.nextInt();
-        String[] stringSets = new String[numOfSet];
-        String tmp;
-
-        int count = 0;
-
-        for(int i = 0; i < numOfSet; i++){
-            stringSets[i] = scanner.next();
-        }
-
-        for(int i = 0; i < numOfStrings; i++){
-            tmp = scanner.next();
-            for(int j = 0; j < numOfSet; j++){
-                if (stringSets[j].equals(tmp)){
-                    count++;
-                }
-            }
-        }
-
-        System.out.println(count);
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int M = Integer.parseInt(input[1]);
+		Set<String> set = new HashSet<>();
+		for (int i = 0; i < N; i++) {
+			String s = br.readLine();
+			set.add(s);
+		}
+		int count = 0;
+		for (int i = 0; i < M; i++) {
+			String s = br.readLine();
+			if (set.contains(s)) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
 }
