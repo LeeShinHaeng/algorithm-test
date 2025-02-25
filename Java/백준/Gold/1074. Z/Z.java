4633156682bd71f802bd64aea8290bb36a2b1f95
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2025-02-25 09:42:06 +0900

    [Gold V] Title: Z, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- Java/백준/Gold/1074. Z/Z.java
+++ Java/백준/Gold/1074. Z/Z.java
@@ -3,45 +3,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+	static int weight;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String[] s = br.readLine().split(" ");
 		int n = Integer.parseInt(s[0]);
 		int r = Integer.parseInt(s[1]);
 		int c = Integer.parseInt(s[2]);
-
 		int len = (int) Math.pow(2, n);
-		System.out.println(recursiveCalc(len, r, c, 0));
+		weight = 0;
+		recursiveCalc(len, r, c);
+		System.out.println(weight);
 	}
 
-	private static int recursiveCalc(int len, int r, int c, int weight) {
+	private static void recursiveCalc(int len, int r, int c) {
 		// base case
 		if (len == 2) {
 			if (r == 0) {
 				if (c == 0)
-					return weight;
-				return weight + 1;
+					return;
+				weight += 1;
+				return;
 			}
-			if (c == 0)
-				return weight + 2;
-			return weight + 3;
+			if (c == 0) {
+				weight += 2;
+				return;
+			}
+			weight += 3;
+			return;
 		}
 
 		int center = len / 2;
 
 		// 좌상단
-		if (center > r && center > c)
-			return recursiveCalc(center, r, c, weight);
-
+		if (center > r && center > c) {
+			recursiveCalc(center, r, c);
+			return;
+		}
 		// 우상단
-		if (center > r)
-			return recursiveCalc(center, r, c - center, weight + (center * center));
-
+		if (center > r) {
+			weight += (center * center);
+			recursiveCalc(center, r, c - center);
+			return;
+		}
 		// 좌하단
-		if (center > c)
-			return recursiveCalc(center, r - center, c, weight + 2 * (center * center));
-
+		if (center > c) {
+			weight += 2 * (center * center);
+			recursiveCalc(center, r - center, c);
+			return;
+		}
 		// 우하단
-		return recursiveCalc(center, r - center, c - center, weight + 3 * (center * center));
+		weight += 3 * (center * center);
+		recursiveCalc(center, r - center, c - center);
+		return;
 	}
 }
