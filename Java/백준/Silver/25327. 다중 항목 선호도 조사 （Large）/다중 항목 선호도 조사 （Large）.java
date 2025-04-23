c729966bb097fb6b3116f4a6cded7732a8a75b82
이신행 <72925577+LeeShinHaeng@users.noreply.github.com>
2025-04-23 09:15:06 +0900

    [Silver III] Title: 다중 항목 선호도 조사 (Large), Time: 612 ms, Memory: 95032 KB -BaekjoonHub

--- Java/백준/Silver/25327. 다중 항목 선호도 조사 （Large）/다중 항목 선호도 조사 （Large）.java
+++ Java/백준/Silver/25327. 다중 항목 선호도 조사 （Large）/다중 항목 선호도 조사 （Large）.java
@@ -5,57 +5,60 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+	static Map<String, Integer> subjects;
+	static Map<String, Integer> fruits;
+	static Map<String, Integer> colors;
+	static BufferedReader br;
+
 	public static void main(String[] args) throws IOException {
 		int[][][] arr = new int[4][4][4];
-		Map<String, Integer> subjects = new HashMap<>();
-		subjects.put("kor", 0);
-		subjects.put("eng", 1);
-		subjects.put("math", 2);
-		subjects.put("-", 3);
-		Map<String, Integer> fruits = new HashMap<>();
-		fruits.put("apple", 0);
-		fruits.put("pear", 1);
-		fruits.put("orange", 2);
-		fruits.put("-", 3);
-		Map<String, Integer> colors = new HashMap<>();
-		colors.put("red", 0);
-		colors.put("blue", 1);
-		colors.put("green", 2);
-		colors.put("-", 3);
+		initMap();
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br = new BufferedReader(new InputStreamReader(System.in));
 		String[] s = br.readLine().split(" ");
 		int N = Integer.parseInt(s[0]);
 		int M = Integer.parseInt(s[1]);
+
 		for (int i = 0; i < N; i++) {
-			s = br.readLine().split(" ");
-			String tmpSubject = s[0];
-			String tmpFruit = s[1];
-			String tmpColor = s[2];
-			int a = subjects.get(tmpSubject);
-			int b = fruits.get(tmpFruit);
-			int c = colors.get(tmpColor);
-			arr[a][b][c]++;
-			arr[3][b][c]++;
-			arr[a][3][c]++;
-			arr[a][b][3]++;
-			arr[a][3][3]++;
-			arr[3][b][3]++;
-			arr[3][3][c]++;
+			int[] nums = readInput();
+			arr[nums[0]][nums[1]][nums[2]]++;
+			arr[3][nums[1]][nums[2]]++;
+			arr[nums[0]][3][nums[2]]++;
+			arr[nums[0]][nums[1]][3]++;
+			arr[nums[0]][3][3]++;
+			arr[3][nums[1]][3]++;
+			arr[3][3][nums[2]]++;
 			arr[3][3][3]++;
 		}
 
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M; i++) {
-			s = br.readLine().split(" ");
-			String tmpSubject = s[0];
-			String tmpFruit = s[1];
-			String tmpColor = s[2];
-			int a = subjects.get(tmpSubject);
-			int b = fruits.get(tmpFruit);
-			int c = colors.get(tmpColor);
-			sb.append(arr[a][b][c]).append("\n");
+			int[] nums = readInput();
+			sb.append(arr[nums[0]][nums[1]][nums[2]]).append("\n");
 		}
 		System.out.println(sb);
+	}
+
+	private static void initMap() {
+		subjects = new HashMap<>();
+		subjects.put("kor", 0);
+		subjects.put("eng", 1);
+		subjects.put("math", 2);
+		subjects.put("-", 3);
+		fruits = new HashMap<>();
+		fruits.put("apple", 0);
+		fruits.put("pear", 1);
+		fruits.put("orange", 2);
+		fruits.put("-", 3);
+		colors = new HashMap<>();
+		colors.put("red", 0);
+		colors.put("blue", 1);
+		colors.put("green", 2);
+		colors.put("-", 3);
+	}
+
+	public static int[] readInput() throws IOException {
+		String[] s = br.readLine().split(" ");
+		return new int[]{subjects.get(s[0]), fruits.get(s[1]), colors.get(s[2])};
 	}
 }
